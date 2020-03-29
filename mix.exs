4d4869e493c146c52a10fe9220ffbc57961209b9
Joel Kemp <mrjoelkemp@gmail.com>
2020-03-29 14:32:17 -0400

    Add description and package information

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Plugwebmetrics.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -15,6 +17,21 @@ defmodule Plugwebmetrics.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp description do
+    """
+    Basic telemetry metrics for a Plug-based, Elixir backend
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Joel Kemp"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mrjoelkemp/plug-web-metrics"}
     ]
   end
 
