c3b89b761596900d788c7d40351a98ce0f75347f
Joel Kemp <mrjoelkemp@gmail.com>
2020-03-29 14:34:00 -0400

    Start with 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Plugwebmetrics.MixProject do
   def project do
     [
       app: :plugwebmetrics,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
